920e869040cac87237fc0256a2d2673a8eee2562
Jh700wolf <148020631+Jh700wolf@users.noreply.github.com>
2023-11-29 19:01:42 -0600

    Update Mundo.cpp

--- Mundo.cpp
+++ Mundo.cpp
@@ -1,47 +1,69 @@
-#ifndef MUNDO_H_
-#define MUNDO_H_
+#ifndef MUNDO_CPP_
+#define MUNDO_CPP_
+#include "Mundo.h"
+#include <sstream>
 
-#include <iostream>
-#include <String>
-#include <Vector>
-#include <fstream>
-#include "Evento.cpp"
-#include "Lugar.cpp"
-#include "Jugador.cpp"
-#include "NoJugador.cpp"
-#include "Personaje.cpp"
+Mundo::Mundo(){
+    nombre="Mundo";
+    descripcionBasica="Añadir texto";
+}
 
+Mundo::Mundo(string _nombre, string _descripcionBasica){
+    nombre = _nombre;
+    descripcionBasica = _descripcionBasica;
+}
+string Mundo::getNombre(){
+    return nombre;
+}
+string Mundo::getDescripcionBasica(){
+    return descripcionBasica;
+}
+string Mundo:: get_mundoInfo(){
+    stringstream aux;
+    aux << "Nombre: " << nombre << endl;
+    aux << "Descripcion general: " << descripcionBasica << endl;
+    aux << "Eventos: " << endl;
+    for (const Evento& evento : eventos){
+        aux << "Evento:" << endl;
+        aux << "Nombre del evento: " << evento.getNombre() << endl;
+    }
+    aux << "Lugares" << endl;
+    for (const Lugar& lugar : lugares){
+        aux << "Lugar:" << endl;
+        aux << "Nombre del evento: " << lugar.getNombre() << endl;
+    }
+    aux << "Jugadores" << endl;
+    for (const Jugador& jugador : jugadores){
+        aux << "Personaje Jugable" << endl;
+        aux << "Nombre del evento: " << jugador.getNombrePersonaje() << endl;
+    }
+    aux << "NPCs: " << endl;
+    for (const NoJugador& noJugador : personajes){
+        aux << "NPC: " << endl;
+        aux << "Nombre del evento: " << noJugador.getNombrePersonaje() << endl;     
+    }
+    return aux.str();
+}
+void Mundo :: set_nombre(string _nombre){
+    nombre = _nombre;
+}
 
-using namespace std;
+void Mundo :: set_descripcion(string _descripcion){
+    descripcionBasica = _descripcion;
+}
+void Mundo :: añadirEvento(Evento _evento){
+    eventos.push_back(_evento);
+}
 
-class Mundo
-{
-    protected:
-        string nombre;
-        string descripcionBasica;
-        vector <Evento> eventos;
-        vector <Lugar> lugares;
-        vector <Jugador> jugadores;
-        vector <NoJugador> personajes;
-        
-    public:
-        Mundo();
-        Mundo(string nombre, string descripcionBasica);
-        string get_mundoInfo();
-        string getNombre();
-        string getDescripcionBasica();
-        void set_descripcion(string descripcion);
-        void set_nombre(string nombre);
-        void añadirEvento(Evento eventoAgregado);
-        void añadirLugar(Lugar lugarAgregado);
-        void añadirJugador(Jugador jugadorAgregado);
-        void añadirPersonaje(NoJugador njAgregado);
+void Mundo :: añadirLugar(Lugar _lugar){
+    lugares.push_back(_lugar);
+}
 
-        
+void Mundo :: añadirJugador(Jugador _jugador){
+    jugadores.push_back(_jugador);
+}
 
-
-
-
-};
-
+void Mundo :: añadirPersonaje(NoJugador _noJugador){
+    personajes.push_back(_noJugador);
+}
 #endif
