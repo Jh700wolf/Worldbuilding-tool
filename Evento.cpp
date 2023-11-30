87265ce192de43d69cd87de6529fe419b10b968a
Jh700wolf <148020631+Jh700wolf@users.noreply.github.com>
2023-11-29 18:57:32 -0600

    Update Evento.cpp

--- Evento.cpp
+++ Evento.cpp
@@ -1,22 +1,43 @@
+#ifndef EVENTO_CPP_
+#define EVENTO_CPP_
 #include "Evento.h"
 #include <sstream>
 
-evento :: evento(){
+Evento :: Evento(){
     nombre = "Evento";
     fecha = "0/0/0";
     descripcion = "Añadir texto";
 }
 
-evento :: evento(string _nombre, string _fecha, string _descripcion){
+Evento :: Evento(string _nombre, string _fecha, string _descripcion){
     nombre = _nombre;
     fecha = _fecha;
     descripcion = _descripcion;
 }
 
-string evento :: get_eventoInfo(){
+string Evento :: getNombre() const{
+    return nombre;
+}
+string Evento :: getFecha(){
+    return fecha;
+}
+string Evento :: getDescripcion(){
+    return descripcion;
+}
+void Evento :: setNombre(string _nombre){
+    nombre = _nombre;
+}
+void Evento :: setFecha(string _fecha){
+    fecha = _fecha;
+}
+void Evento :: setDescripcion(string _descripcion){
+    descripcion = _descripcion;
+}
+string Evento :: getEventoInfo(){
     stringstream aux;
     aux << "Nombre del evento: " << nombre << endl;
     aux << "Fecha: " << fecha << endl;
     aux << "Descripción: " << descripcion << endl;
     return aux.str();
 }
+#endif
